d4b6231698c15402747a3dc668018b29c2e8dc5f
Ugi Ispoyo Widodo <ugi.ispoyowidodo@gmail.com>
2019-03-21 15:14:21 +0700

    update router delete and select

--- api/models/products.js
+++ api/models/products.js
@@ -4,7 +4,9 @@ const mongoose = require('mongoose');
 const productSchema = mongoose.Schema({
     _id: mongoose.Schema.Types.ObjectId,
     name: String,
-    price: Number
+    price: Number,
+    created_at: {type: Date, default: Date.now},
+    updated_at: {type: Date, default: Date.now}
 });
 
 module.exports = mongoose.model('Product', productSchema);
--- api/routes/products.js
+++ api/routes/products.js
@@ -1,13 +1,46 @@
 const express = require('express');
 const router = express.Router();
+const url = require('url');  
+const querystring = require('querystring');  
 const mongoose = require('mongoose');
 
 const Product = require('./../models/products');
 
 router.get('/', (req, res, next) => {
-    res.status(200).json({
-        message: 'this products'
-    });
+    let parsedUrl = url.parse(req.originalUrl);  
+    let parsedQs = querystring.parse(parsedUrl.query);
+    let limit = 10;
+    let offset = 0;
+    let orderby = -1;
+    if(parsedQs.limit !== undefined && parsedQs.offset !== undefined){
+        limit = parseInt(parsedQs.limit);
+        offset = parseInt(parsedQs.offset);
+    }
+    if(parsedQs.orderby !== undefined) {
+        orderby = parsedQs.orderby
+    }
+    /* Count all data */
+    let count = Product.count();
+    Product.find()
+        .limit(limit)
+        .sort({created_at: orderby})
+        .skip(offset)
+        .exec()
+        .then(result => {
+            Promise.resolve(count)
+                .then(datacount => {
+                    res.status(200).json({
+                        data: result,
+                        total_data: datacount
+                    });
+                })
+        })
+        .catch(e => {
+            console.log(e);
+            res.status(500).json({
+                error: e
+            });
+        });
 });
 
 router.post('/', (req, res, next) => {
@@ -16,15 +49,19 @@ router.post('/', (req, res, next) => {
         name: req.body.name,
         price: req.body.price
     })
-    product.save().then(result => {
-        console.log(result);
-    }).catch(e => {
-        console.log(e);
-    })
-    res.status(201).json({
-        message: 'this post products',
-        createdProduct: product
-    });
+    product.save()
+        .then(result => {
+            console.log(result);
+            res.status(201).json({
+                message: 'data successful added',
+                createdProduct: product
+            });
+        }).catch(e => {
+            console.log(e);
+            res.status(500).json({
+                error: e
+            });
+        });
 });
 
 router.get('/:id', (req, res, next) => {
@@ -33,7 +70,13 @@ router.get('/:id', (req, res, next) => {
         .exec()
         .then(result => {
             console.log(result);
-            res.status(200).json(result);
+            if(result){
+                res.status(200).json(result);
+            } else {
+                res.status(404).json({
+                    message: "Data not found!"
+                })
+            }
         })
         .catch(e => {
             console.log(e);
@@ -48,9 +91,17 @@ router.patch('/:id', (req, res, next) => {
 });
 
 router.delete('/:id', (req, res, next) => {
-    res.status(200).json({
-        message: 'this products '+ req.params.id
-    });
+    const id = req.params.id;
+    Product.remove({ _id: id })
+        .exec()
+        .then(result => {
+            res.status(200).json(result);
+        })
+        .catch(e => {
+            res.status(500).json({
+                error: e
+            })
+        });
 });
 
 
