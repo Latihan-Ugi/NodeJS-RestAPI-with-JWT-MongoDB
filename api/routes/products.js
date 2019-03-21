1ba65cc0e57f49f49af72bdcce2917e486d9115a
Ugi Ispoyo Widodo <ugi.ispoyowidodo@gmail.com>
2019-03-21 21:22:10 +0700

    update

--- api/routes/products.js
+++ api/routes/products.js
@@ -133,7 +133,7 @@ router.patch('/:id', (req, res, next) => {
 
 router.delete('/:id', (req, res, next) => {
     const id = req.params.id;
-    Product.remove({ _id: id })
+    Product.deleteOne({ _id: id })
         .exec()
         .then(result => {
             res.status(200).json(result);
